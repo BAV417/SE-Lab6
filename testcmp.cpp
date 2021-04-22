ba69f33068f0ca1f3409a90b1cd4911bdf546dd3
BAV <ke1717bav34@susu.ru>
2021-04-22 19:31:14 +0500

    New testcmp.cpp

--- testcmp.cpp
+++ testcmp.cpp
@@ -5,9 +5,9 @@ int main(){
  	Complex A, B(-4), C(23.0, 45.9);
 	cout << A << "," << B << "," << C << endl;
 	A=B+C;
-	cout << "A=B+C,A=" << A << endl;
+	cout << "A=B+C ,A=" << A << endl;
 	Complex M = B - C;
-	cout << "M = B - C, M = " << M << endl;
+	cout << "M=B-C, M = " << M << endl;
 	cout << "M*A=" << M*A << endl;
 	cout << "M/4.45=" << M/4.45 << endl;
         Complex D;
